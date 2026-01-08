e2af01d6a7b8a5babe841a1f2878ce9103823b0b
User Name <usermail@gmail.com>
2026-01-08 23:12:36 +0530

    Add trie and input validation

--- wordle.py
+++ wordle.py
@@ -6,9 +6,38 @@ from nltk.corpus import words
 nltk.download('words')
 allWords = words.words()
 fiveLetterWords = [w.upper() for w in allWords if len(w)==5]
+class Node:
+    def __init__(self):
+        self.arr = [None for i in range(26)]
+        self.flag = False
+    
+class Trie:
+    def __init__(self):
+        self.head = Node()
+    
+    def pushWord(self, word : str):
+        temp = self.head
+        for char in word:
+            i = ord(char) - ord('A')
+            if temp.arr[i] == None:
+                temp.arr[i] = Node()
+            temp = temp.arr[i]
+        temp.flag = True
+
+    def findWord(self, word : str) -> bool:
+        temp = self.head
+        for char in word:
+            i = ord(char) - ord('A')
+            if temp.arr[i] == None:
+                return False
+            temp = temp.arr[i]
+        return temp.flag
+
+database = Trie()
+for wd in fiveLetterWords:
+    database.pushWord(wd)
+
 word = random.choice(fiveLetterWords)
-
-
 letters = {}
 for char in word:
     if letters.get(char) == None:
@@ -19,9 +48,25 @@ print(word)
 
 done = False
 while (done==False) :
-    guess = input().upper()
-    print("\033[1A\033[2K\r", end="") #this is so that it writes in place
     init(autoreset=True)
+    guess = input().upper()
+
+    if len(guess) != 5:
+        print("\033[1A\033[2K\r", end="")
+        print(Fore.RED + "Word is not 5 letters long", end = "\r")
+        continue
+
+    try:
+        if not database.findWord(guess):
+            print("\033[1A\033[2K\r", end="") #this is so that it writes in place
+            print(Fore.RED + "Not in word list", end = "\r")
+            continue
+    except:
+        print("\033[1A\033[2K\r", end="") #this is so that it writes in place
+        print(Fore.RED + "Only alphabets allowed", end = "\r")
+        continue
+
+    print("\033[1A\033[2K\r", end="")
     currLetters = letters.copy()
     cols = [None for _ in range(5)]
     for i  in range(5):
